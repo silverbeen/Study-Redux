ecc52abdbf7b0fa23c376656856a2f307d9c6cc6
강은빈 <kub0803@naver.com>
2021-02-14 01:02:27 +0900

    리액트 없이 쓰는 리덕스

---
+++ redux 공부하기 (순수 리덕스만)/src/index.js
@@ -0,0 +1,85 @@
+import { createStore } from "redux";
+
+const lightDiv = document.getElementsByClassName("light")[0];
+const switchButton = document.getElementById("switch-btn");
+
+const counterHeadings = document.getElementsByTagName("h1")[0];
+const plusButton = document.getElementById("plus-btn");
+const minusButton = document.getElementById("minus-btn");
+
+//console.log( {lightDiv,switchButton,counterHeadings,plusButton,minusButton} );
+
+//액션 타입 정의
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+// 액션 생성함수 정의
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increment = diff => ({ type: INCREMENT, diff });
+const decrement = () => ({ type: DECREMENT });
+
+// 초기값 설정
+
+const initialState = {
+  light: false,
+  counter: 0
+};
+
+//리듀서 함수 정의
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        light: !state.light
+      };
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + action.diff
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+// **** render 함수 만들기
+const render = () => {
+  const state = store.getState(); // 현재 상태를 가져옵니다.
+  const { light, counter } = state; // 편의상 비구조화 할당
+  if (light) {
+    lightDiv.style.background = "green";
+    switchButton.innerText = "끄기";
+  } else {
+    lightDiv.style.background = "gray";
+    switchButton.innerText = "켜기";
+  }
+  counterHeadings.innerText = counter;
+};
+
+render();
+
+store.subscribe(render);
+
+switchButton.onclick = () => {
+  store.dispatch(toggleSwitch());
+  console.log(1);
+};
+
+plusButton.onclick = () => {
+  store.dispatch(increment(5));
+};
+
+minusButton.onclick = () => {
+  store.dispatch(decrement());
+};
